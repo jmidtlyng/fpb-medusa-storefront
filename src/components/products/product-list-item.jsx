575fb4e73bacd723a9329128a1e83e3d599d3f3a
yk <jacobmidtlyng@gmail.com>
2023-03-01 00:13:34 -0800

    remove horzontal scrollbars and add scroller arrows when there are more images to horizontally scroll. Needs scroll on click function

--- src/components/products/product-list-item.jsx
+++ src/components/products/product-list-item.jsx
@@ -80,6 +80,24 @@ const ProductListItem = ({ product, prodCount, prodPosition }) => {
                 <img src={img.url}
                     alt={product.title}
                     loading="lazy"/>
+                {images.length > 0 && images.length - 1 > i &&
+                  <button className="gallery-item-page-control gallery-item-page-control--right"
+                          style={{transform: 'rotate(90deg)'}}
+                          onClick={() => scrollToPrev()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                      <path d="M8.2 275.4c0-8.6 3.4-17.401 10-24.001 13.2-13.2 34.8-13.2 48 0l451.8 451.8 445.2-445.2c13.2-13.2 34.8-13.2 48 0s13.2 34.8 0 48L542 775.399c-13.2 13.2-34.8 13.2-48 0l-475.8-475.8c-6.8-6.8-10-15.4-10-24.199z"/>
+                    </svg>
+                  </button>
+                }
+                {i > 0 &&
+                  <button className="gallery-item-page-control gallery-item-page-control--left"
+                          style={{transform: 'rotate(170deg)'}}
+                          onClick={() => scrollToPrev()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                      <path d="M8.2 275.4c0-8.6 3.4-17.401 10-24.001 13.2-13.2 34.8-13.2 48 0l451.8 451.8 445.2-445.2c13.2-13.2 34.8-13.2 48 0s13.2 34.8 0 48L542 775.399c-13.2 13.2-34.8 13.2-48 0l-475.8-475.8c-6.8-6.8-10-15.4-10-24.199z"/>
+                    </svg>
+                  </button>
+                }
               </div>
             )}
         </div>
